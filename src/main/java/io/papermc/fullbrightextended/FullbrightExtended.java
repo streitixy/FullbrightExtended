a71e9707eaed56940c6eae47b736dea9c75b1b33
streitixy <danilomeiraivo@gmail.com>
2024-04-26 18:23:06 -0300

    Revert "1.3.2"
    This reverts commit 702e6044d5b874b65680d0beada898104b013f64.

--- src/main/java/io/papermc/fullbrightextended/FullbrightExtended.java
+++ src/main/java/io/papermc/fullbrightextended/FullbrightExtended.java
@@ -70,93 +70,89 @@ class CommandDetector implements CommandExecutor {
     public CommandDetector(FullbrightExtended plugin) {
         this.plugin = plugin;
     }
-
     private String sendMessage(String language, String messageKey) {
 
         return plugin.getConfig().getString(language + "." + messageKey);
     }
 
 
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("fullbright")) {
             if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (args.length == 0) {
-                    PotionEffect effect = player.getPotionEffect(PotionEffectType.NIGHT_VISION);
-                    if (effect == null) {
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
-                        sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_on"));
-                    } else {
-                        player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                        sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off"));
-                    }
-                } else if (args.length == 1) {
-                    Player target = Bukkit.getPlayerExact(args[0]);
-                    if (target != null) {
-                        if (sender.hasPermission("FullbrightExtended.command.fullbrightextendedarguments")) {
-                            PotionEffect effect = target.getPotionEffect(PotionEffectType.NIGHT_VISION);
-                            if (effect == null) {
-                                target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
-                                sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_on_other").replace("{player}", args[0]));
-                            } else {
-                                target.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off_other").replace("{player}", args[0]));
-                            }
-                        } else {
-                            sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "permission"));
-                        }
-                    } else {
-                        sender.sendMessage(prefix + ChatColor.GOLD + sendMessage(language, "fb_player_not_found").replace("{player}", args[0]));
-                    }
-                } else if (args.length == 2) {
-                    Player target = Bukkit.getPlayerExact(args[0]);
-                    if (target != null) {
-                        if (sender.hasPermission("FullbrightExtended.command.fullbrightextendedarguments")) {
-                            PotionEffect effect = target.getPotionEffect(PotionEffectType.NIGHT_VISION);
-                            if (effect == null) {
-                                if (args[1].equals("off")) {
-                                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off_other").replace("{player}", args[0]));
-                                } else if (args[1].equals("on")) {
-                                    target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
-                                    sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_off_other").replace("{player}", args[0]));
-                                } else {
-                                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_state"));
-                                }
-                            } else {
-                                if (args[1].equals("off")) {
-                                    target.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                                    sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_already_on_other").replace("{player}", args[0]));
-                                } else if (args[1].equals("on")) {
-                                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_alreay_off_other").replace("{player}", args[0]));
-                                } else {
-                                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_state"));
-                                }
-                            }
-                        } else {
-                            sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "permission"));
-                        }
-                    } else {
-                        sender.sendMessage(prefix + ChatColor.GOLD + sendMessage(language, "fb_player_not_found").replace("{player}", args[0]));
-                    }
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                PotionEffect effect = player.getPotionEffect(PotionEffectType.NIGHT_VISION);
+                if (effect == null) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+                    sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_on"));
+                } else {
+                    player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off"));
                 }
-                if (args.length > 2) {
-                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_toomuch"));
-                    return false;
+            } else if (args.length == 1) {
+                Player target = Bukkit.getPlayerExact(args[0]);
+                if (target != null) {
+                    if (sender.hasPermission("FullbrightExtended.command.fullbrightextendedarguments")) {
+                        PotionEffect effect = target.getPotionEffect(PotionEffectType.NIGHT_VISION);
+                        if (effect == null) {
+                            target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+                            sender.sendMessage(prefix + ChatColor.GREEN +  sendMessage(language, "fb_on_other").replace("{player}", args[0]));
+                        } else {
+                            target.removePotionEffect(PotionEffectType.NIGHT_VISION);
+                            sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off_other").replace("{player}", args[0]));
+                        }
+                    } else {
+                        sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "permission"));
+                    }
+                } else {
+                    sender.sendMessage(prefix + ChatColor.GOLD + sendMessage(language, "fb_player_not_found").replace("{player}", args[0]));
+                }
+            } else if (args.length == 2) {
+                Player target = Bukkit.getPlayerExact(args[0]);
+                if (target != null) {
+                    if (sender.hasPermission("FullbrightExtended.command.fullbrightextendedarguments")) {
+                        PotionEffect effect = target.getPotionEffect(PotionEffectType.NIGHT_VISION);
+                        if (effect == null) {
+                            if (args[1].equals("off")) {
+                                sender.sendMessage(prefix + ChatColor.RED +  sendMessage(language, "fb_off_other").replace("{player}", args[0]));
+                            } else if (args[1].equals("on")) {
+                                target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+                                sender.sendMessage(prefix + ChatColor.GREEN +  sendMessage(language, "fb_off_other").replace("{player}", args[0]));
+                            } else {
+                                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_state"));
+                            }
+                        } else {
+                            if (args[1].equals("off")) {
+                                target.removePotionEffect(PotionEffectType.NIGHT_VISION);
+                                sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_already_on_other").replace("{player}", args[0]));
+                            } else if (args[1].equals("on")) {
+                                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_alreay_off_other").replace("{player}", args[0]));
+                            } else {
+                                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_state"));
+                            }
+                        }
+                    } else {
+                        sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "permission"));
+                    }
+                } else {
+                    sender.sendMessage(prefix + ChatColor.GOLD + sendMessage(language, "fb_player_not_found").replace("{player}", args[0]));
                 }
             }
-            else {
-                sender.sendMessage(prefix + ChatColor.RED + language, "console");
-
+            if (args.length > 2) {
+                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_toomuch"));
+                return false;
             }
         }
-
-            else if (cmd.getName().equalsIgnoreCase("fblanguage"))
-            {
-                if (args[0].equals("pt_BR")) {
+            else if (cmd.getName().equalsIgnoreCase("fblanguage")) {
+                if (args[0].equals("pt_BR")){
                     plugin.getConfig().set("language", "pt_BR");
 
-                } else if (args[0].equals("en_US")) {
+                }
+
+
+                else if (args[0].equals("en_US")){
                     plugin.getConfig().set("language", "en_US");
                 }
                 sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "language_changed"));
@@ -164,8 +160,13 @@ class CommandDetector implements CommandExecutor {
 
 
             }
+        }
+        else{
+            sender.sendMessage( prefix + ChatColor.RED + language, "console");
+        }
+        return true;
 
-        return true;}
+    }
 }
 
 
