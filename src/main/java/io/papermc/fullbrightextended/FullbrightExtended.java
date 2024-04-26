bbe84f0c5d1072d021bc640305d0674b43e2af4a
streitixy <danilomeiraivo@gmail.com>
2024-04-26 17:23:42 -0300

    v1.3
    added language to config file
    added pt_BR

--- src/main/java/io/papermc/fullbrightextended/FullbrightExtended.java
+++ src/main/java/io/papermc/fullbrightextended/FullbrightExtended.java
@@ -6,34 +6,46 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static io.papermc.fullbrightextended.FullbrightExtended.prefix;
 
+public string sendMessage(string language, string messageKey){
+    if (language.equals("en_US")){
+        return(getConfig().getString(language+"."+messageKey))
 
+
+
+    }
+
+}
 public class FullbrightExtended extends JavaPlugin {
 
     private ConfigManager configManager;
+    private LanguageManager languageManager;
     public static String prefix;
+    public static String language;
 
     @Override
     public void onEnable() {
         configManager = new ConfigManager(this);
         configManager.setupConfig();
+        languageManager = new LanguageManager(this);
+        languageManager.setupConfig();
 
         prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("prefix", "&6[FullbrightExtended] "));
+        language = getConfig().getString("language", "en_US");
 
-        this.getCommand("fullbright").setExecutor(new FullbrightCommand());
+        this.getCommand("fullbright").setExecutor(new Command());
         this.getCommand("fullbright").setTabCompleter(new FullbrightCommandTabCompleter());
+
+        this.getCommand("fblanguage").setExecutor(new Command());
+        this.getCommand("fblanguage").setTabCompleter(new LanguageCommand());
+
 
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[FullbrightExtended]: Plugin is enabled");
     }
@@ -45,20 +57,21 @@ public class FullbrightExtended extends JavaPlugin {
 }
 
 
-class FullbrightCommand implements CommandExecutor {
+class Command implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, @NotNull Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
+        if (cmd.getName().equalsIgnoreCase("fullbright")) {
+            if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 0) {
                 PotionEffect effect = player.getPotionEffect(PotionEffectType.NIGHT_VISION);
                 if (effect == null) {
                     player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
-                    sender.sendMessage(prefix + ChatColor.GREEN + "Turned fullbright on for you.");
+                    sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_on"));
                 } else {
                     player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                    sender.sendMessage(prefix + ChatColor.RED + "Turned fullbright off for you.");
+                    sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off"));
                 }
             } else if (args.length == 1) {
                 Player target = Bukkit.getPlayerExact(args[0]);
@@ -67,13 +80,13 @@ class FullbrightCommand implements CommandExecutor {
                         PotionEffect effect = target.getPotionEffect(PotionEffectType.NIGHT_VISION);
                         if (effect == null) {
                             target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
-                            sender.sendMessage(prefix + ChatColor.GREEN + "Turned fullbright on for " + args[0] + ".");
+                            sender.sendMessage(prefix + ChatColor.GREEN +  sendMessage(language, "fb_on_other").replace("{player}", args[0]));
                         } else {
                             target.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                            sender.sendMessage(prefix + ChatColor.RED + "Turned fullbright off for " + args[0] + ".");
+                            sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_off_other").replace("{player}", args[0]));
                         }
                     } else {
-                        sender.sendMessage(prefix + ChatColor.RED + "You do not have permission to use this command.");
+                        sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "permission");
                     }
                 } else {
                     sender.sendMessage(prefix + ChatColor.GOLD + "The player " + args[0] + " is not online/does not exist!");
@@ -85,58 +98,51 @@ class FullbrightCommand implements CommandExecutor {
                         PotionEffect effect = target.getPotionEffect(PotionEffectType.NIGHT_VISION);
                         if (effect == null) {
                             if (args[1].equals("off")) {
-                                sender.sendMessage(prefix + ChatColor.RED + "Fullbright is already off for " + args[0] + "!");
+                                sender.sendMessage(prefix + ChatColor.RED +  sendMessage(language, "fb_off_other").replace("{player}", args[0]));
                             } else if (args[1].equals("on")) {
                                 target.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
-                                sender.sendMessage(prefix + ChatColor.GREEN + "Turned fullbright on for " + args[0] + ".");
+                                sender.sendMessage(prefix + ChatColor.GREEN +  sendMessage(language, "fb_off_other").replace("{player}", args[0]));
                             } else {
                                 sender.sendMessage(prefix + ChatColor.RED + "Parameter 'state' invalid.");
                             }
                         } else {
                             if (args[1].equals("off")) {
                                 target.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                                sender.sendMessage(prefix + ChatColor.GREEN + "Turned fullbright off for " + args[0] + ".");
+                                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "fb_already_on_other").replace("{player}", args[0]));
                             } else if (args[1].equals("on")) {
-                                sender.sendMessage(prefix + ChatColor.RED + "Fullbright is already on for " + args[0] + "!");
+                                sender.sendMessage(prefix + ChatColor.GREEN + sendMessage(language, "fb_alreay_off_other").replace("{player}", args[0]));
                             } else {
-                                sender.sendMessage(prefix + ChatColor.RED + "Parameter 'state' invalid.");
+                                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_state"));
                             }
                         }
                     } else {
-                        sender.sendMessage(prefix + ChatColor.RED + "You do not have permission to use this command.");
+                        sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "permission"));
                     }
                 } else {
-                    sender.sendMessage(prefix + ChatColor.GOLD + "The player " + args[0] + " is not online/does not exist!");
+                    sender.sendMessage(prefix + ChatColor.GOLD + sendMessage(language, "fb_player_not_found").replace("{player}", args[0]));
                 }
             }
             if (args.length > 2) {
-                sender.sendMessage(prefix + ChatColor.RED + "You added too many parameters.");
+                sender.sendMessage(prefix + ChatColor.RED + sendMessage(language, "parameter_toomuch"));
                 return false;
             }
         }
-        return true;
-    }
-}
+            else if (cmd.getName().equalsIgnoreCase("fblanguage")) {
+                if (args[0].equals("pt_BR")){
+                    getConfig().set("language", "pt_BR");
+                }
+
+                else if (args[0].equals("en_US")){
+                    getConfig().set("language", "en_US");
+                }
 
 
-class FullbrightCommandTabCompleter implements TabCompleter {
 
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (args.length == 1) {
-            // If the first argument, return a list of online players
-            return Bukkit.getOnlinePlayers().stream()
-                    .map(player -> player.getName())
-                    .collect(Collectors.toList());
-        } else if (args.length == 2) {
-            // If the second argument, return a list of "on" and "off"
-            List<String> list = new ArrayList<>();
-            list.add("on");
-            list.add("off");
-            return list;
+            }
         }
+        return true;
 
-        // If more than two arguments, return an empty list
-        return new ArrayList<>();
     }
 }
+
+
---
+++ src/main/java/io/papermc/fullbrightextended/LanguageManager.java
@@ -0,0 +1,53 @@
+package io.papermc.fullbrightextended;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LanguageManager {
+
+    private final JavaPlugin plugin;
+    private File configFile;
+    private FileConfiguration config;
+
+    public ConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void setupConfig() {
+        configFile = new File(plugin.getDataFolder(), "languages.yml");
+        if (!configFile.exists()) {
+            plugin.saveResource("languages.yml", false);
+        }
+
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public FileConfiguration getConfig() {
+        if (config == null) {
+            setupConfig();
+        }
+        return config;
+    }
+
+    public void saveConfig() {
+        if (config == null || configFile == null) {
+            return;
+        }
+        try {
+            getConfig().save(configFile);
+        } catch (IOException ex) {
+            plugin.getLogger().warning("Could not save config to " + configFile.getName());
+        }
+    }
+
+    public void reloadConfig() {
+        if (configFile == null) {
+            configFile = new File(plugin.getDataFolder(), "languages.yml");
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+}
